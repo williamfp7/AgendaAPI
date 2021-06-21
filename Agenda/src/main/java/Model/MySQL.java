8f4adabb757fd316c0b1d94d5d4e4729c4030012
willi <willi@DESKTOP-4SR3EEP>
2021-06-21 12:04:46 -0300

    Todos os arquivos criados

---
+++ Agenda/src/main/java/Model/MySQL.java
@@ -0,0 +1,13 @@
+package Model;
+
+import java.sql.Connection;
+
+public class MySQL implements DataBase {
+
+	@Override
+	public Connection getcon() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
